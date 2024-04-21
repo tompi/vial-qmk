1bdc0f84f985de23ba2ef09ab3705b88069aea71
Thomas Haukland <thomas.haukland@gmail.com>
2024-04-21 21:28:56 +0200

    8 layers and remove warnings

--- keyboards/cheapino/config.h
+++ keyboards/cheapino/config.h
@@ -24,10 +24,6 @@
 #define ENCODER_RESOLUTION 1
 
 #define DEBOUNCE 5
-#define VIAL_KEYBOARD_UID {0x79, 0xEC, 0x46, 0x42, 0x81, 0xA0, 0xD8, 0x55}
-// Unlock combo is two inner thumb keys.
-#define VIAL_UNLOCK_COMBO_ROWS { 2, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 5, 11 }
 
 // These are just dummy definitions to make qmk compile
 // The actual encoder logic is handled manually in encoder.c
---
+++ keyboards/cheapino/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#define VIAL_KEYBOARD_UID {0x79, 0xEC, 0x46, 0x42, 0x81, 0xA0, 0xD8, 0x55}
+// Unlock combo is two inner thumb keys.
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 5, 11 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
